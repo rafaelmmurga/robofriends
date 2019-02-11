0decfb8bf103000103958b2df2de1b9cdd9eefd7
Rafael Murga <rafaelmmurga@gmail.com>
2019-02-11 11:16:30 -0600

    Updates

---
+++ precache-manifest.996e208d65e704bf2ec6550de17ad765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1dfa527ef733611d4df7",
+    "url": "/robofriends/static/js/main.1dfa527e.chunk.js"
+  },
+  {
+    "revision": "3b857247057a9e535f62",
+    "url": "/robofriends/static/js/1.3b857247.chunk.js"
+  },
+  {
+    "revision": "1dfa527ef733611d4df7",
+    "url": "/robofriends/static/css/main.323add8e.chunk.css"
+  },
+  {
+    "revision": "3b857247057a9e535f62",
+    "url": "/robofriends/static/css/1.1623a8c7.chunk.css"
+  },
+  {
+    "revision": "67436e8fe8232647273e0f827fa6652b",
+    "url": "/robofriends/index.html"
+  }
+];
