1219fbcf986a99c7e0c13f8e4a505b504565e3ef
Rafael Murga <rafaelmmurga@gmail.com>
2019-02-11 14:23:26 -0600

    update api

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		fetch('https://blackburzum.github.io/robofriends/users.json')
+		fetch('https://blackburzum.github.io/datos/users.json')
 		.then(response => response.json())
 		.then(users    => 	this.setState({robots: users}) )
 		
